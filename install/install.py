fe560e196aaf158f4fed6e56cac9d4f641b7f184
Itzik Paz <spideron@gmail.com>
2025-01-09 00:22:12 +0000

    work in progress

--- install/install.py
+++ install/install.py
@@ -19,61 +19,82 @@ checks_config = json.load(checks_config_file)
 iam_policy_file = open(cwd + '/lambdas/iam_policy.json')
 iam_policy_document = iam_policy_file.read().replace('REGION_NAME', region).replace('ACCOUNT_ID', account_id)
 iam_trusted_relationship_file = open(cwd + '/lambdas/iam_trust_relationship.json')
+iam_trusted_relationship_document = iam_trusted_relationship_file.read()
+
+# Helper method to get a name with prefix from the config
+def get_name_with_prefix(name):
+    return '{}_{}'.format(install_config['prefix'], name)
 
 # Initialize DynamoDB objects
-ddb_checks_table = ddb.DynamoDBHandler(install_config['prefix'] + install_config['dynamodb']['tables']['checks'], region)
-ddb_settings_table = ddb.DynamoDBHandler(install_config['prefix'] + install_config['dynamodb']['tables']['settings'], region)
-ddb_log_table = ddb.DynamoDBHandler(install_config['prefix'] + install_config['dynamodb']['tables']['log'], region)
+ddb_checks_table = ddb.DynamoDBHandler(get_name_with_prefix(install_config['dynamodb']['tables']['checks']), region)
+ddb_settings_table = ddb.DynamoDBHandler(get_name_with_prefix(install_config['dynamodb']['tables']['settings']), region)
+ddb_log_table = ddb.DynamoDBHandler(get_name_with_prefix(install_config['dynamodb']['tables']['log']), region)
 
-# Delete tables if they exists
-if ddb_checks_table.table_exists():
-    ddb_checks_table.delete_dynamodb_table()
+# Initialize IAM object
+iam_handler = iam.IAMHandler(region, account_id)
+
+# # Delete tables if they exists
+# if ddb_checks_table.table_exists():
+#     ddb_checks_table.delete_dynamodb_table()
     
-if ddb_settings_table.table_exists():
-    ddb_settings_table.delete_dynamodb_table()
+# if ddb_settings_table.table_exists():
+#     ddb_settings_table.delete_dynamodb_table()
 
-# Create tables
-ddb_checks_table.create_dynamodb_table()
-ddb_settings_table.create_dynamodb_table()
+# # Create tables
+# ddb_checks_table.create_dynamodb_table()
+# ddb_settings_table.create_dynamodb_table()
 
-# The logs table do net get re-created in order to keep old logs
-if not ddb_log_table.table_exists():
-    ddb_log_table.create_dynamodb_table()
+# # The logs table do net get re-created in order to keep old logs
+# if not ddb_log_table.table_exists():
+#     ddb_log_table.create_dynamodb_table()
 
-# Populate the checks table
-for m in checks_config['modules']:
-    item_module = m['name']
-    item_version = m['version']
+# # Populate the checks table
+# for m in checks_config['modules']:
+#     item_module = m['name']
+#     item_version = m['version']
     
-    for c in m['checks']:
-        item_uuid = uuid.uuid4()
-        item_id = str(item_uuid)
-        item_name = c['name']
-        item_template = c['template']
+#     for c in m['checks']:
+#         item_uuid = uuid.uuid4()
+#         item_id = str(item_uuid)
+#         item_name = c['name']
+#         item_template = c['template']
         
-        item={
-                "id": {'S': item_id},
-                "name": {'S': item_name},
-                "version": {'S': item_version},
-                "module": {'S': item_module},
-                "template_message": {'S': item_template},
-                "enabled": {'BOOL' : True},
-                "muted": {'BOOL': False}
-            }
+#         item={
+#                 "id": {'S': item_id},
+#                 "name": {'S': item_name},
+#                 "version": {'S': item_version},
+#                 "module": {'S': item_module},
+#                 "template_message": {'S': item_template},
+#                 "enabled": {'BOOL' : True},
+#                 "muted": {'BOOL': False}
+#             }
         
-        ddb_checks_table.put_item(item)
+#         ddb_checks_table.put_item(item)
 
-# Populate the settings table
-item_uuid = uuid.uuid4()
-item_id = str(item_uuid)
-item = {
-    "id": {'S': item_id},
-    "subscriber": {'S': ''},
-    "schedule": {'S': ''}
-    }
-ddb_settings_table.put_item(item)
+# # Populate the settings table
+# item_uuid = uuid.uuid4()
+# item_id = str(item_uuid)
+# item = {
+#     "id": {'S': item_id},
+#     "subscriber": {'S': ''},
+#     "schedule": {'S': ''}
+#     }
+# ddb_settings_table.put_item(item)
 
-# Create IAM policy and role
+print(iam_policy_document)
+# Create IAM policy and role do be used by Lambda functions
+iam_policy_arn = iam_handler.create_iam_policy(
+    get_name_with_prefix(install_config['iam']['dynamoDBPolicyName']), 
+    iam_policy_document,
+    'Allow access to the {} DynamoDB tables'.format(install_config['prefix'])
+)
+iam_role_arn = iam_handler.create_iam_role_and_attach_role(
+    get_name_with_prefix(install_config['iam']['lambdaRoleName']),
+    iam_trusted_relationship_document,
+    iam_policy_arn,
+    'A role for {} Lambda working with DynamoDB'.format(install_config['prefix'])
+)
+iam_handler.attach_lambda_basic_policy(install_config['iam']['lambdaRoleName'])
 
 
 # # Clear old Lambda zips
